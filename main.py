cca0d68b6543926f068ff369082593cf2c9b850c
berdibaevich <berdibaevich.me@gmail.com>
2025-08-13 22:40:23 +0500

    Add threading support for multiple clients

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import socket
+from threading import Thread
+
+
+def handle_command(client):
+    while True:
+        req = client.recv(1024)
+        if not req:
+            break
+        client.sendall(b"+PONG\r\n")
+
 
 def main():
     server_socket = socket.create_server(("localhost", 6379))
     while True:
         conn, _ = server_socket.accept() # Wait a client
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-
-            conn.sendall(b"+PONG\r\n")
+        Thread(target=handle_command, args=(conn,)).start()
 
 
 if __name__ == "__main__":
